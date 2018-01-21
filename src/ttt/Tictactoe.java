b60594513d1c2c9b4a615449be5d372e73cd2e17
nagasthi <nxa162430@utdallas.edu>
2018-01-21 09:51:04 -0600

    First Commit

--- src/ttt/Tictactoe.java
+++ src/ttt/Tictactoe.java
@@ -2,23 +2,24 @@ package ttt;
 
 public class TicTacToe {
 
-	public static void main(String[] args) {
-		GameBoard game = new GameBoard();
-		int counter = 1;
-		game.clearBoard();
-		game.displayBoard();
-		while (game.gameActive() && counter < 10) {
-			if (counter % 2 == 0) {
-				game.askPlayer('O');
-			} else {
-				game.askPlayer('X');
-			}
-			counter++;
-			game.displayBoard();
-			game.checkForWinner();
-			if (counter == 10) {
-				System.out.println("Game Draw");
-			}
-		}
-	}
+    public static void main(String[] args) {
+        GameBoard game = new GameBoard();
+        int counter = 1;
+        game.clearBoard();
+        game.displayBoard();
+        while (game.gameActive() && counter < 10) {
+            if (counter % 2 == 0) {
+                game.askPlayer('O');
+            } else {
+                game.askPlayer('X');
+            }
+            counter++;
+            game.displayBoard();
+            game.checkForWinner();
+            if (counter == 10 && !game.checkForWinner()) {
+                System.out.println("Game Draw");
+            }
+        }
+        game.closeBoard();
+    }
 }
